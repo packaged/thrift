c662baa1e9818f0e458b07813674b7ba71f9ac69
Tom Kay <oridan82@gmail.com>
2015-01-23 18:16:46 +0000

    update from upstream

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
-git clone --depth=1 --branch=master git://git.apache.org/thrift.git tmp
+git clone --depth=1 --branch=master git@github.com:apache/thrift tmp
 cp -R tmp/lib/php/lib/* src/
 rm -Rf tmp
